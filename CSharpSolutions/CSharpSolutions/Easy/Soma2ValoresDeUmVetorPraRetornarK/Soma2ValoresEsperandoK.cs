d3b7d304f5ad34d61f312940965e56d408b060c2
Felipe Rodrigues da Silva <felipe_rodrigues-silva@outlook.com>
2020-06-25 18:10:54 -0300

    Acrescentando metodo de forca bruta

--- CSharpSolutions/CSharpSolutions/Easy/Soma2ValoresDeUmVetorPraRetornarK/Soma2ValoresEsperandoK.cs
+++ CSharpSolutions/CSharpSolutions/Easy/Soma2ValoresDeUmVetorPraRetornarK/Soma2ValoresEsperandoK.cs
@@ -58,6 +58,24 @@ namespace CSharpSolutions.Easy
         }
 
 
+        public int[] BruteForce(int[] nums, int target)
+        {
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                for(int j = i +1; j< nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == target)
+                    {
+                        return new int[] { i, j };
+                    }
+                }
+            }
+
+            throw new ArgumentException();
+        }
+
+
         
 
 
